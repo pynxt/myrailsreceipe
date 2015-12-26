3d72125571ebd1af84bf56b66ef0b9081927319e
pynxt <pynxt@yahoo.com>
2015-12-26 23:45:51 +0000

    added associations and validations

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+   
+   def setup
+    @recipe = Recipe.new(name:"chicken parm", summary:"this is the best chicken parm ever",
+              description: "heat oil add sugar salt add tomato sauce and heat for 20 mins")
+   end
+
+   test "recipe should be valid " do
+       assert @recipe.valid?
+   end
+   
+   test "name should be present " do 
+       @recipe.name =" "
+       assert_not @recipe.valid?
+   end       
+   
+    
+end
